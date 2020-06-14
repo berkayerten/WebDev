aa273fa3226ce70b6f23baf08f295460c9aba0f6
Erten <berkay.erten.ext@siemens.com>
2020-06-14 04:16:58 +0300

    Updates

---
+++ precache-manifest.c39779a99108f4202e7933acace6b4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5516bb428018c7009ffcca4a4f2613b",
+    "url": "/WebDev/index.html"
+  },
+  {
+    "revision": "b25f86d852ce7b8847c6",
+    "url": "/WebDev/static/css/main.71cbe6d1.chunk.css"
+  },
+  {
+    "revision": "9b395d666d52a966843a",
+    "url": "/WebDev/static/js/2.b447098d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebDev/static/js/2.b447098d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25f86d852ce7b8847c6",
+    "url": "/WebDev/static/js/main.64641f85.chunk.js"
+  },
+  {
+    "revision": "9445a06807a5d3c05922",
+    "url": "/WebDev/static/js/runtime-main.4457e184.js"
+  }
+]);
